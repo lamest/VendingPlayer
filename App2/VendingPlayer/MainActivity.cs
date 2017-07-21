6bfc3d906f2ab1028ba946791ec2bce41b20600a
Ivan Suhih <i.suhih@di-group.biz>
2017-07-21 11:33:41 +0700

    final cuts

---
+++ App2/VendingPlayer/MainActivity.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using Android.App;
+using Android.Media;
+using Android.OS;
+using Android.Preferences;
+using Android.Views;
+using Android.Widget;
+using Compass.FilePicker;
+using Uri = Android.Net.Uri;
+
+namespace VendingPlayer
+{
+    [Activity(Label = "VendingPlayer", MainLauncher = true, Icon = "@drawable/icon", Theme = "@android:style/Theme.Black.NoTitleBar.Fullscreen")]
+    public class MainActivity : Activity
+    {
+        private const string _filePathKey = "FilePath";
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            //final PowerManager pm = (PowerManager)getSystemService(Context.POWER_SERVICE);
+            //this.mWakeLock = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK, "My Tag");
+            //this.mWakeLock.acquire();
+            SetContentView(Resource.Layout.Main);
+
+            Window.AddFlags(WindowManagerFlags.KeepScreenOn |
+                            WindowManagerFlags.DismissKeyguard |
+                            WindowManagerFlags.ShowWhenLocked |
+                            WindowManagerFlags.TurnScreenOn |
+                            WindowManagerFlags.Fullscreen);
+
+
+            var videoView = FindViewById<VideoView>(Resource.Id.MainVideoView);
+
+            SetFile(videoView);
+            videoView.Start();
+            videoView.Completion += OnCompletion;
+            videoView.Error += OnError;
+            videoView.Touch += OnTouch;
+            videoView.KeepScreenOn = true;
+        }
+
+        private void OnTouch(object sender, View.TouchEventArgs e)
+        {
+            if (e.Event.Action == MotionEventActions.Up)
+            {
+                var videoView = sender as VideoView;
+
+                ShowFilePickerDialog(videoView);
+            }
+        }
+
+        private void SetFile(VideoView videoView, string path = null)
+        {
+            string filePath;
+            if (path == null)
+            {
+                filePath = GetDefaultFilePath();
+            }
+            else
+            {
+                filePath = path;
+            }
+            var uri = Uri.Parse(filePath);
+            videoView.SetVideoURI(uri);
+        }
+
+        private string GetDefaultFilePath()
+        {
+            var prefs = PreferenceManager.GetDefaultSharedPreferences(ApplicationContext);
+            var filePath = string.Empty;
+            if (prefs.All.ContainsKey(_filePathKey))
+            {
+                filePath = (string) prefs.All[_filePathKey];
+            }
+
+            return filePath;
+        }
+
+        private void OnError(object sender, MediaPlayer.ErrorEventArgs e)
+        {
+            var videoView = sender as VideoView;
+
+            ShowFilePickerDialog(videoView);
+        }
+
+        private void ShowFilePickerDialog(VideoView videoView)
+        {
+            var filePickerFragment = new FilePickerFragment();
+            filePickerFragment.FileSelected += (sender1, path) =>
+            {
+                filePickerFragment.Dismiss();
+                SetFile(videoView, path);
+                SaveFilePath(path);
+                videoView.Start();
+            };
+            filePickerFragment.Cancel += sender2 => filePickerFragment.Dismiss();
+            filePickerFragment.Show(FragmentManager, "FilePicker");
+        }
+
+        private void SaveFilePath(string path)
+        {
+            var prefs = PreferenceManager.GetDefaultSharedPreferences(ApplicationContext);
+            var editor = prefs.Edit();
+            editor.PutString(_filePathKey, path);
+            editor.Apply();
+        }
+
+        private void OnCompletion(object sender, EventArgs e)
+        {
+            var videoView = sender as MediaPlayer;
+            videoView?.Start();
+        }
+    }
+}
--- App2/VendingPlayer/Properties/AssemblyInfo.cs
+++ App2/VendingPlayer/Properties/AssemblyInfo.cs
@@ -1,16 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using Android.App;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("VendingPlayer_Android")]
+[assembly: AssemblyTitle("VendingPlayer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("VendingPlayer_Android")]
+[assembly: AssemblyProduct("VendingPlayer")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
